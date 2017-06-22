f6140a3438992c474cd77d891769cff1c207a1ce
Martin Dlouhy <md@robotika.cz>
2017-06-22 06:48:18 +0200

    Experiment with global multiprocessing.Pool

--- lib/processor.py
+++ lib/processor.py
@@ -2,60 +2,40 @@
 """
   Parallel processor/worker/filter for collected sensor data
 """
-from multiprocessing import Process, Queue
+from multiprocessing import Pool
 
 
-g_index = 0
-g_queue_out = None
-g_processor = None
-g_queue_in = None
-
-
-def process_main(queue_in, queue_out, process_fn):
-    global g_index
-    while True:
-        data = queue_in.get()
-        print "processing ...", data
-        if data is None:
-            break
-        ret = process_fn(data)  # TODO replace by parameter
-        queue_out.put_nowait((g_index, ret))
-        g_index += 1
-        print "waiting ..."
+g_pool = None
 
 
 class Processor:
     def __init__(self, process_fn):
-        # process_fn is now ignored - how to pass it to second interpreter??
-        global g_queue_out, g_processor, g_queue_in
-        g_queue_out = Queue()
-        g_queue_in = Queue()
-        self.processing = False
-        g_processor = Process(target=process_main,
-                              args=(g_queue_out, g_queue_in, process_fn))
+        global g_pool
+        g_pool = Pool(processes=1)
+        self.process_fn = process_fn
+        self.processing = None
 
     def get_result(self):
-        global g_queue_in
-        if g_queue_in.empty():
+        global g_pool
+        if self.processing is None or self.processing.ready():
             return None
-        ret = g_queue_in.get()
-        self.processing = False
+        ret = self.processing.get()
+        self.processing = None
         return ret
 
     def start(self):
-        global g_processor
-        g_processor.start()
+        pass  # only legacy function
 
     def requestStop(self):
-        global g_queue_out
-        g_queue_out.put_nowait(None)
+        global g_pool
+        g_pool.close()
+
 
     def push_back(self, data):
-        global g_queue_out
+        global g_pool
         print "Processor", data
         if not self.processing:
-            self.processing = True
-            g_queue_out.put_nowait(data)
+            self.processing = g_pool.apply_async(self.process_fn, (data,))
         else:
             print "Skipped", data
 
