39001490e7aa8b9c099914a9d3749775b2aa520a
Martin Dlouhy <md@robotika.cz>
2017-06-22 06:53:44 +0200

    Test class version of Pool instance

--- lib/processor.py
+++ lib/processor.py
@@ -5,18 +5,13 @@
 from multiprocessing import Pool
 
 
-g_pool = None
-
-
 class Processor:
     def __init__(self, process_fn):
-        global g_pool
-        g_pool = Pool(processes=1)
+        self.pool = Pool(processes=1)
         self.process_fn = process_fn
         self.processing = None
 
     def get_result(self):
-        global g_pool
         if self.processing is None or self.processing.ready():
             return None
         ret = self.processing.get()
@@ -27,15 +22,12 @@ class Processor:
         pass  # only legacy function
 
     def requestStop(self):
-        global g_pool
-        g_pool.close()
-
+        self.pool.close()
 
     def push_back(self, data):
-        global g_pool
         print "Processor", data
-        if not self.processing:
-            self.processing = g_pool.apply_async(self.process_fn, (data,))
+        if self.processing is None:
+            self.processing = self.pool.apply_async(self.process_fn, (data,))
         else:
             print "Skipped", data
 
