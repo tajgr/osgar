84737330f36e3d739e58e01dd1c953999a46f550
Martin Dlouhy <md@robotika.cz>
2018-11-11 20:47:13 +0100

    Add driver for Cortexpilot

---
+++ osgar/drivers/cortexpilot.py
@@ -0,0 +1,50 @@
+"""
+  Driver for robot Robik from cortexpilot.com
+"""
+
+import struct
+from threading import Thread
+
+from osgar.bus import BusShutdownException
+
+
+class Cortexpilot(Thread):
+    def __init__(self, config, bus):
+        Thread.__init__(self)
+        self.setDaemon(True)
+
+        self.bus = bus
+        self._buf = b''
+
+        self.desired_speed = 0.0  # m/s
+        self.desired_angular_speed = 0.0
+        self.flags = 0x40  # laser ON, TODO
+
+        self.emergency_stop = None  # uknown state
+        self.pose = (0.0, 0.0, 0.0)  # x, y in meters, heading in radians (not corrected to 2PI)
+
+    def create_packet(self):
+        packet = struct.pack('<ffI', self.desired_speed,
+                             self.desired_angular_speed, self.flags)
+        assert len(packet) < 256, len(packet)  # just to use LSB only
+        ret = bytes([0, 0, len(packet) + 2 + 1, 0x1, 0x0C]) + packet
+        checksum = sum(ret)
+        return ret + bytes([256-checksum])
+
+    def run(self):
+        try:
+            self.bus.publish('raw', self.make_cmd())
+            while True:
+                dt, channel, data = self.bus.listen()
+                if channel == 'raw':
+                    self._buf += data
+                    if len(self._buf) > 16:  # hack
+                        self._buf = b''
+                        self.bus.publish('raw', self.create_packet())
+        except BusShutdownException:
+            pass
+
+    def request_stop(self):
+        self.bus.shutdown()
+
+# vim: expandtab sw=4 ts=4
---
+++ osgar/drivers/test_cortexpilot.py
@@ -0,0 +1,30 @@
+import unittest
+
+from osgar.drivers.cortexpilot import Cortexpilot
+from osgar.bus import BusHandler
+
+
+class EduroTest(unittest.TestCase):
+
+    def Xtest_usage(self):
+        q = MagicMock()
+        logger = MagicMock()
+        logger.write = MagicMock(return_value=135)
+        bus = BusHandler(logger=logger,
+                out={'can': [], 'encoders': [], 'emergency_stop': [],
+                     'pose2d': [(q, 'pose2d'),], 'buttons': []})
+        eduro = Eduro(config={}, bus=bus)
+        sync = CAN_packet(0x80, [])
+        bus.queue.put((123, 'can', sync))
+        bus.shutdown()
+        eduro.run()
+        q.put.assert_called_once_with((135, 'pose2d', [0, 0, 0]))
+
+    def test_create_packet(self):
+        robot = Cortexpilot(config={}, bus=None)
+        packet = robot.create_packet()
+        self.assertEqual(len(packet), 3 + 15)
+        self.assertEqual(sum(packet) % 256, 0)
+        self.assertEqual(packet[-1], 0xa4)
+
+# vim: expandtab sw=4 ts=4
