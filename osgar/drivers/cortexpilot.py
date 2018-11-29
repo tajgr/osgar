9844a3da5e9c4c2fae5d76ec40f9589dfdd1b1b0
Martin Dlouhy <md@robotika.cz>
2018-11-29 06:17:58 +0100

    cortexpilot.py - parse packet frames

--- osgar/drivers/cortexpilot.py
+++ osgar/drivers/cortexpilot.py
@@ -28,8 +28,26 @@ class Cortexpilot(Thread):
                              self.desired_angular_speed, self.flags)
         assert len(packet) < 256, len(packet)  # just to use LSB only
         ret = bytes([0, 0, len(packet) + 2 + 1, 0x1, 0x0C]) + packet
-        checksum = sum(ret)
+        checksum = sum(ret) & 0xFF
         return ret + bytes([256-checksum])
+
+    def get_packet(self):
+        """extract packet from internal buffer (if available otherwise return None"""
+        data = self._buf
+        if len(data) < 5:
+            return None
+        high, mid, low = data[:3]  # packet length
+        assert high == 0, high  # all messages < 65535 bytes
+        size = 256 * mid + low + 3  # counting also 3 bytes of len header
+        if len(data) < size:
+            return None
+        ret, self._buf = data[:size], data[size:]
+        checksum = sum(ret) & 0xFF
+        assert checksum == 0, checksum  # checksum error
+        return ret
+
+    def parse_packet(self, data):
+        pass
 
     def run(self):
         try:
@@ -38,9 +56,11 @@ class Cortexpilot(Thread):
                 dt, channel, data = self.bus.listen()
                 if channel == 'raw':
                     self._buf += data
-                    if len(self._buf) > 16:  # hack
-                        self._buf = b''
+                    packet = self.get_packet()
+                    if packet is not None:
+                        self.parse_packet(packet)
                         self.bus.publish('raw', self.create_packet())
+
         except BusShutdownException:
             pass
 
