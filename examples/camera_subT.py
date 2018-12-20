e78551c99ed43157cabdfb4b2ee3df7c91dfc203
tajgr <levjakub@gmail.com>
2018-12-20 23:42:51 +0100

    just improved parameters

--- examples/camera_subT.py
+++ examples/camera_subT.py
@@ -17,9 +17,9 @@ except ImportError:
 
 from osgar.logger import LogReader, lookup_stream_id
 from osgar.lib.serialize import deserialize
+#from matplotlib import pyplot as plt
 
-
-KERNEL = np.ones((10, 10), np.uint8)
+KERNEL = np.ones((5, 5), np.uint8)
 #yi, ye, xi, xe = 350, 550, 200, 1080
 
 
@@ -28,6 +28,7 @@ def showImg( img ):
     cv2.resizeWindow('image', 600,600)
     cv2.imshow( 'image', img )
     cv2.waitKey(0)
+    #time.sleep(0.1)
     cv2.destroyAllWindows()
     
     
@@ -38,14 +39,15 @@ def red_firefighter(img, ind = 0):
     r = r.astype(float)
     mat = b + g + r
     mat[mat == 0] = 1.0  # avoid division by 0
-    r[r < 20] = 0        # ignore 'dark red'
+    r[r < 150] = 0        # ignore 'dark red'
     reddish = r/mat*255  # 'normalize' red, i.e. red compared to other color planes
     reddish = reddish.astype(np.uint8)
     #plt.hist( reddish.ravel(), 256,[0,256])
     #plt.show()
+    #plt.savefig("im/hist_%s" % str(ind), dpi=100)
     
     #cv2.imwrite( "im/gray_%04d.png" % ind, reddish )
-    __, binaryImg = cv2.threshold(reddish, 160, 255, cv2.THRESH_BINARY)
+    __, binaryImg = cv2.threshold(reddish, 120, 255, cv2.THRESH_BINARY)
     
     binaryImg = cv2.morphologyEx(binaryImg, cv2.MORPH_CLOSE, KERNEL)
     binaryImg = cv2.morphologyEx(binaryImg, cv2.MORPH_OPEN, KERNEL)
@@ -53,7 +55,7 @@ def red_firefighter(img, ind = 0):
     ___, contours, ___ = cv2.findContours( binaryImg, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
     contours2 = []
     for cnt in contours:
-        if cv2.contourArea(cnt) > 500:
+        if cv2.contourArea(cnt) > 100:
             contours2.append(cnt)
     
     ret = []
@@ -68,17 +70,18 @@ def camera_processing(logfile):
     only_stream = lookup_stream_id(logfile, 'rosmsg_image.image') #or "camera.raw"
     with LogReader(logfile) as log:
         for timestamp, stream_id, data in log.read_gen(only_stream):
-            print(timestamp)
+            #print(timestamp)
             buf = deserialize(data)
             img = cv2.imdecode(np.fromstring(buf, dtype=np.uint8), 1)
             #print(img.shape)
-            artefacts = red_firefighter(img)
+            artefacts = red_firefighter(img, ind = timestamp)
             if len(artefacts) > 0:
                 for x,y,w,h in artefacts:
                     cv2.rectangle(img, (x,y),(x+w, y+h), (0, 0, 255),3)
             
-                print(artefacts)
-                showImg( img )
+                print(timestamp, artefacts)
+                #showImg( img )
+                #cv2.imwrite("im/im_%s.png" % str(timestamp), img)
 
 
 if __name__ == "__main__":
