f1363e90f14ae8eb32ee3272bf8fa99641117adb
Jakub <jlev@tf.czu.cz>
2018-12-21 11:56:55 +0100

    red firefighter detection ver1

--- examples/camera_subT.py
+++ examples/camera_subT.py
@@ -4,6 +4,7 @@
 """
 from datetime import timedelta
 import sys
+import time
 
 try:
     import cv2
@@ -29,17 +30,18 @@ def showImg( img ):
     cv2.imshow( 'image', img )
     cv2.waitKey(0)
     #time.sleep(0.1)
-    cv2.destroyAllWindows()
+    #cv2.destroyAllWindows()
     
     
 def red_firefighter(img, ind = 0):
+    m, n, __ = img.shape
     b,g,r = cv2.split(img)
     b = b.astype(float)
     g = g.astype(float)
     r = r.astype(float)
     mat = b + g + r
     mat[mat == 0] = 1.0  # avoid division by 0
-    r[r < 150] = 0        # ignore 'dark red'
+    r[r < 120] = 0        # ignore 'dark red'
     reddish = r/mat*255  # 'normalize' red, i.e. red compared to other color planes
     reddish = reddish.astype(np.uint8)
     #plt.hist( reddish.ravel(), 256,[0,256])
@@ -52,18 +54,38 @@ def red_firefighter(img, ind = 0):
     binaryImg = cv2.morphologyEx(binaryImg, cv2.MORPH_CLOSE, KERNEL)
     binaryImg = cv2.morphologyEx(binaryImg, cv2.MORPH_OPEN, KERNEL)
     
-    ___, contours, ___ = cv2.findContours( binaryImg, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
+    __, contours, __ = cv2.findContours( binaryImg, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
     contours2 = []
     for cnt in contours:
-        if cv2.contourArea(cnt) > 100:
+        area = cv2.contourArea(cnt)
+        if area > 100 and area < m*n/16:
             contours2.append(cnt)
     
-    ret = []
+    b_rect = []
     for cnt in contours2:
         x,y,w,h = cv2.boundingRect(cnt)
-        ret.append([x, y, w, h])
+        b_rect.append([x, y, w, h])
     
+    ret = []
+    if len(b_rect) == 1:
+        x, y, w, h = b_rect[0]
+        if ( w/h > 1/6 ) and ( w/h < 1/2 ): # only red firefighter in the standing position
+            ret = b_rect
+            
+    elif len(b_rect) == 2: #checking size and position
+        x1, y1, w1, h1 = b_rect[0]
+        x2, y2, w2, h2 = b_rect[1]
+        if abs(w1*h1 - w2*h2) < max( w1*h1, w2*h2) *0.5:
+            if abs(w1-w2) < max( w1, w2) *0.2:
+                if abs(x1 - x2) < max( w1, w2) *0.2:
+                    if abs(y1 - y2) < max( h1, h2) *3:
+                        ret.append([ min(x1, x2), min(y1, y2), abs(x1-x2) + min(w1, w2), abs(y1 -y2) + min(h1, h2) ])
+    
+    elif len(b_rect) >2:
+        pass #maybe next time
+        
     return ret
+    
 
 
 def camera_processing(logfile):
@@ -80,9 +102,9 @@ def camera_processing(logfile):
                     cv2.rectangle(img, (x,y),(x+w, y+h), (0, 0, 255),3)
             
                 print(timestamp, artefacts)
-                #showImg( img )
+                showImg( img )
                 #cv2.imwrite("im/im_%s.png" % str(timestamp), img)
-
+        cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
